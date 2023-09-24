866c613ca474d197424eee165888100dae3ccf5e
RanganadhaKishore <145891229+RanganadhaKishore@users.noreply.github.com>
2023-09-24 12:28:48 +0530

    Update my.js

--- my.js
+++ my.js
@@ -1,5 +1,4 @@
 const url = "https://script.google.com/macros/s/AKfycbzc1KbpbjZWXHdyrIhOSDYRmXZ2cA6xVbsrJDW4zxjgcnt2DuXMc660zBTytkBC8K1B/exec"
-
 function getJsonData(){
     fetch(url).then(d=>d.json()).then(d=>{
         document.getElementById("app").textContent= d[0].status
@@ -11,13 +10,11 @@ function sendJsonData(){
         method: "POST", // *GET, POST, PUT, DELETE, etc.
         mode: "no-cors", // no-cors, *cors, same-origin
         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        // credentials: "same-origin", // include, *same-origin, omit
+        credentials: "omit", // include, *same-origin, omit
         headers: {
           "Content-Type": "application/json",
-          // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         redirect: "follow", // manual, *follow, error
-        // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify({name: "Ranganadha"}), // body data type must match "Content-Type" header
       })
     }
