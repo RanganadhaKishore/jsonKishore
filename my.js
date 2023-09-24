04c32750adbf432f0d011ce886b6050399898740
RanganadhaKishore <145891229+RanganadhaKishore@users.noreply.github.com>
2023-09-24 12:43:52 +0530

    Update my.js

--- my.js
+++ my.js
@@ -1,25 +1,21 @@
 const url = "https://script.google.com/macros/s/AKfycbzc1KbpbjZWXHdyrIhOSDYRmXZ2cA6xVbsrJDW4zxjgcnt2DuXMc660zBTytkBC8K1B/exec"
+
 function getJsonData(){
     fetch(url).then(d=>d.json()).then(d=>{
         document.getElementById("app").textContent= d[0].status
     })
 }
 
+document.getElementById("btn").addEventListener("click", getJsonData)
+
 function sendJsonData(){
     fetch(url,{
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "no-cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        /*
-        credentials: "omit", // include, *same-origin, omit
-        headers: {
-          "Content-Type": "application/json",
-        },
-        */
-        redirect: "follow", // manual, *follow, error
-        body: JSON.stringify({name: "Palepu"}), // body data type must match "Content-Type" header
+        method: "POST", 
+        mode: "no-cors", 
+        cache: "no-cache", 
+        redirect: "follow", 
+        body: JSON.stringify({name: "Palepu"}), 
       })
     }
 
-document.getElementById("btn").addEventListener("click", getJsonData)
 document.getElementById("btn1").addEventListener("click", sendJsonData)
