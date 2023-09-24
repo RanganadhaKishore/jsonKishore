bcc72d2d613e9a31a5c5e3779c94b30935278460
RanganadhaKishore <145891229+RanganadhaKishore@users.noreply.github.com>
2023-09-24 12:39:31 +0530

    Update my.js

--- my.js
+++ my.js
@@ -10,10 +10,12 @@ function sendJsonData(){
         method: "POST", // *GET, POST, PUT, DELETE, etc.
         mode: "no-cors", // no-cors, *cors, same-origin
         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "omit", // include, *same-origin, omit
+        //credentials: "omit", // include, *same-origin, omit
+        /*
         headers: {
           "Content-Type": "application/json",
         },
+        */
         redirect: "follow", // manual, *follow, error
         body: JSON.stringify({name: "Ranganadha"}), // body data type must match "Content-Type" header
       })
